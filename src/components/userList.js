c53ae61c5a051e7d1b8f40e335b91d114955fbcc
Mr-vasuyenumula <vasuyenumula213@gmail.com>
2022-12-29 15:08:53 +0530

    frontend

---
+++ src/components/userList.js
@@ -0,0 +1,19 @@
+// array of user objects to be loaded initially for testing
+
+const userList = [{
+    id:0,
+    name : "Jyothi",
+    email: "jyothi@gmail.com", 
+    mobile: "9951031355",
+    password: "fd0abda76d5c674f382ba4043e519470715f0b0e",
+    tasks: [
+        {
+            id_val : 2,
+            completed : false,
+            desc : "Loaded from const task"
+        }
+    ],
+    logged : false
+}]
+
+export default userList
